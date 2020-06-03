94bb9a8f884bd926455bbc443b4b02a98df24cf2
unknown <camiaraquecaro17@gmail.com>
2020-06-03 11:41:35 -0500

    [UPD]Adding Function main Manage Handler and CheckConnection to Data Base

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/AraqueGD/Twittor/bd"
+	"github.com/AraqueGD/Twittor/handlers"
+)
+
+func main() {
+	if bd.CheckConnection() == 0 {
+		log.Fatal("Fail Connection to Data Base")
+		return
+	}
+	handlers.Manage()
 }
