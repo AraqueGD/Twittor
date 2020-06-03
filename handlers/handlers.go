fa1320c3352eabb7acad99718fee48074733290b
unknown <camiaraquecaro17@gmail.com>
2020-06-03 11:43:53 -0500

    [ADD]Function Manage Listen Port

---
+++ handlers/handlers.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+// Manage Set teh PORT and Handler listen Sever.
+func Manage() {
+	router := mux.NewRouter()
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "8080"
+	}
+	handler := cors.AllowAll().Handler(router)
+	log.Fatal(http.ListenAndServe(":"+PORT, handler))
+}
